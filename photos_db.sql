691bdc416075da7b6b6992b1eba44d4abbb05b83
xxxOkamexxx <94180822+xxxOkamexxx@users.noreply.github.com>
2022-02-27 21:12:53 +0100

    add files and filling out the db.

---
+++ photos_db.sql
@@ -0,0 +1,64 @@
+SELECT VERSION();
+
+CREATE DATABASE Photos;
+
+USE Photos;
+
+-- ---------------
+-- CREATE TABLES
+-- ---------------
+
+-- DROP TABLES photos;
+CREATE TABLE photos (
+  id integer NOT NULL AUTO_INCREMENT,
+  title varchar(255) NOT NULL,
+  url varchar(255) DEFAULT NULL,
+  comment varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+-- DROP TABLES albums;
+CREATE TABLE albums (
+  id integer NOT NULL AUTO_INCREMENT,
+  album_title varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+-- DROP TABLES users;
+CREATE TABLE users (
+  id integer NOT NULL AUTO_INCREMENT,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+-- ---------------
+-- INSERT DATA
+-- ---------------
+
+-- photos
+INSERT INTO photos (title, url, comment)
+VALUES
+	('yogi','https://unsplash.com/photos/ZlFKIG6dApg','yoga cat'),
+	('My home','https://unsplash.com/photos/WajTuzeanUk','My Dream Home!'),
+	('Where is my keys?','https://unsplash.com/photos/upRfmqsar8Q','Why do I always lose my keys?'),
+	('Quack','https://unsplash.com/photos/_CTpGHp8nGk','I am not drowning');
+
+
+-- albums
+INSERT INTO albums (album_title)
+VALUES
+	('cute cats'),
+	('funny animals');
+
+-- users
+INSERT INTO users (email, password, first_name, last_name)
+VALUES
+	('johan@mail.se','asdf','Johan','Nordström'),
+	('sean_banan@epost.com','chiquita','Sean','Banan');
+

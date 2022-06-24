d313eaa5dee8bca07bc15e4db893a027ddbdf600
xxxOkamexxx <94180822+xxxOkamexxx@users.noreply.github.com>
2022-06-24 13:42:39 +0200

    Cleanup of Codes

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const userController = require('../controllers/user_controller');
-
-// /* Get all users */
-// router.get('/', userController.getUser);
-
-// module.exports = router;
